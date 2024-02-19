bb397e6d6cb235be0fac3c76fd1598e601ac00db
Abdusamatov-Samandar <abdusamatovsamandardeveloper@gmail.com>
2024-02-19 10:35:31 +0500

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./*.{html, js}"],
+  theme: {
+    extend: {},
+    colors: {
+      "blue": "#5267DF",
+      "red-light": "#FA5959",
+      "dark": "#242A45",
+      "gray": "#9194A2"
+    },
+    fontFamily: {
+      sans: ["Rubik", "sans-serif"]
+    }
+  },
+  plugins: [],
+}
+
